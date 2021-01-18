179463ffa6a7ae2e3f5e37963a6cc5339d2829e0
Łukasz Barylak <lbarylak@pm.me>
2021-01-18 05:43:16 +0100

    working on hero

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Footer() {
 	return (
-		<footer className='bg-blue-900 h-12 w-screen text-center text-yellow-500 text-xl font-bold flex items-center justify-center fixed bottom-0'>
-			©2020 - {(new Date().getFullYear())} Łukasz Barylak & Kamil Duliniec
+		<footer className='bg-black h-12 w-screen text-center text-white text-xl font-bold flex items-center justify-center fixed bottom-0'>
+			©2020 - {(new Date().getFullYear())} Łukasz Barylak
 		</footer>
 	)
 }
--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function Hero() {
 	return (
 		<main>
-			<h1 className='h-screen bg-blue-500'>
+			<h1 className='h-screen bg-green-500'>
 				<div className='p-10'>
-					<span className='text-yellow-500 text-xl font-bold'>Cyb</span>
-					<span className='text-white text-xl font-bold'>Sec</span>
+					<span className='text-white text-xl font-bold'>lbarylak.</span>
+					<span className='text-red-500 text-xl font-bold'>pro</span>
 				</div>
 			</h1>
 		</main>
